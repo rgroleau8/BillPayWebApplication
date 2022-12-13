7c22c8be6bb7bf57a50cde92cecd72b8e6750327
Randy Groleau <groleaurandy@yahoo.com>
2022-12-13 16:17:35 -0500

    Fix namespace of Logger.cs

--- BillPayData/Services/Logger/Logger.cs
+++ BillPayData/Services/Logger/Logger.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BillPayData.Services.Logger
+namespace BillPayData.Services
 {
     public class Logger : ILogger
     {
